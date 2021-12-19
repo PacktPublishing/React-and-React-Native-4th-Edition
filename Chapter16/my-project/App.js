03e2ad5dc22216bf1f115c11da8144faa332c0d3
Mikhail Sakhniuk <sakhnyukmikhail@gmail.com>
2021-12-19 17:52:36 +0500

    Chapter 16

---
+++ Chapter16/my-project/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.js to start working on your!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+  },
+});
