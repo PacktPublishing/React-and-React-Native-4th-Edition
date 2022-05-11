3c817b5fda6535a0ca4c1ea4ddc2610e72ee37ae
Joseph Aloocaran <Josepha@packt.com>
2022-05-11 12:10:53 +0530

    Chapter29

--- Chapter29/web/src/index.js
+++ Chapter29/web/src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: 'http://localhost:4000/graphql',
-});
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+    <BrowserRouter>
       <App />
-    </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
